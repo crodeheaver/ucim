72dcfd5c89b5c2a1cdbd90ebd420589ed877f3dc
crodeheaver <colbyrodeheaver@hotmail.com>
2016-02-19 13:46:06 -0600

    unauthorized redirected to auth/login

--- app/controllers/game.js
+++ app/controllers/game.js
@@ -5,97 +5,97 @@ var Game = mongoose.model('Game')
 var Team = mongoose.model('Team')
 var isLoggedIn = require('../../config/auth')
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use('/game', router)
 }
 
 router.route('/')
-  .get(isLoggedIn, function(req, res, next) {
+  .get(isLoggedIn, function (req, res, next) {
     Game.aggregate([{
-        $sort: {
-          Name: 1
+      $sort: {
+        Name: 1
+      }
+    }, {
+      $group: {
+        _id: '$section',
+        games: {
+          $push: '$$ROOT'
         }
-      }, {
-        $group: {
-          _id: '$section',
-          games: {
-            $push: '$$ROOT'
-          }
-        }
-      }])
+      }
+    }])
       .exec()
-      .then(function(sections) {
+      .then(function (sections) {
         return Team.populate(sections, {
           'path': 'games.loser games.winner'
         })
       })
-      .then(function(sections) {
+      .then(function (sections) {
         res.render('game/index', {
           title: 'Games',
           sections: sections,
           user: req.user
         })
       })
-      .catch(function(err) {
+      .catch(function (err) {
         next(err)
       })
   })
-  .post(isLoggedIn, function(req, res, next) {
+  .post(isLoggedIn, function (req, res, next) {
     var winner = mongoose.Types.ObjectId(req.body.winnerId)
     var loser = mongoose.Types.ObjectId(req.body.loserId)
     Game.remove({
-        _id: req.body.gameId
-      })
+      _id: req.body.gameId
+    })
       .exec()
-      .then(function() {
+      .then(function () {
         return Game.aggregate([{
-            $match: {
-              $or: [{
-                'winner': winner
-              }, {
-                'loser': winner
-              }, {
-                'winner': loser
-              }, {
-                'loser': loser
-              }]
-            }
-          }, {
-            $group: {
-              _id: null,
-              winLoseCount: {
-                '$sum': {
-                  '$cond': [{
-                    $eq: ['$loser', winner]
-                  }, 1, 0]
-                }
-              },
-              winWinCount: {
-                '$sum': {
-                  '$cond': [{
-                    $eq: ['$winner', winner]
-                  }, 1, 0]
-                }
-              },
-              loseLoseCount: {
-                '$sum': {
-                  '$cond': [{
-                    $eq: ['$loser', loser]
-                  }, 1, 0]
-                }
-              },
-              loseWinCount: {
-                '$sum': {
-                  '$cond': [{
-                    $eq: ['$winner', loser]
-                  }, 1, 0]
-                }
+          $match: {
+            $or: [{
+              'winner': winner
+            }, {
+              'loser': winner
+            }, {
+              'winner': loser
+            }, {
+              'loser': loser
+            }]
+          }
+        }, {
+          $group: {
+            _id: null,
+            winLoseCount: {
+              '$sum': {
+                '$cond': [{
+                  $eq: ['$loser', winner]
+                }, 1, 0]
+              }
+            },
+            winWinCount: {
+              '$sum': {
+                '$cond': [{
+                  $eq: ['$winner', winner]
+                }, 1, 0]
+              }
+            },
+            loseLoseCount: {
+              '$sum': {
+                '$cond': [{
+                  $eq: ['$loser', loser]
+                }, 1, 0]
+              }
+            },
+            loseWinCount: {
+              '$sum': {
+                '$cond': [{
+                  $eq: ['$winner', loser]
+                }, 1, 0]
               }
             }
-          }])
+          }
+        }])
           .exec()
       })
-      .then(function(results) {
+      .then(function (results) {
         Team.update({
           _id: loser
         }, {
@@ -110,27 +110,27 @@ router.route('/')
         }, {}).exec()
         res.redirect('/game')
       })
-      .catch(function(err) {
+      .catch(function (err) {
         next(err)
       })
   })
 
 router.route('/addGame')
-  .get(isLoggedIn, function(req, res, next) {
+  .get(isLoggedIn, function (req, res, next) {
     var date = new Date()
     Game.find()
       .populate('winner')
       .populate('loser')
       .exec()
-      .then(function(games) {
+      .then(function (games) {
         return Team.find({
-            section: req.query.section
-          }).exec()
-          .then(function(teams) {
+          section: req.query.section
+        }).exec()
+          .then(function (teams) {
             return [games, teams]
           })
       })
-      .then(function(results) {
+      .then(function (results) {
         var numGames
         if (results[0] === undefined) numGames = 1
         else numGames = results[0].length + 1
@@ -143,72 +143,72 @@ router.route('/addGame')
           user: req.user
         })
       })
-      .catch(function(err) {
+      .catch(function (err) {
         next(err)
       })
   })
-  .post(isLoggedIn, function(req, res, next) {
+  .post(isLoggedIn, function (req, res, next) {
     var winner = mongoose.Types.ObjectId(req.body.winner)
     var loser = mongoose.Types.ObjectId(req.body.loser)
 
     new Game({
-        gameName: req.body.gameName,
-        winner: req.body.winner,
-        winnerPoints: req.body.winnerPoints,
-        loser: req.body.loser,
-        loserPoints: req.body.loserPoints,
-        section: req.body.section,
-        date: new Date(req.body.date + ' ' + req.body.time)
-      }).save()
-      .then(function() {
+      gameName: req.body.gameName,
+      winner: req.body.winner,
+      winnerPoints: req.body.winnerPoints,
+      loser: req.body.loser,
+      loserPoints: req.body.loserPoints,
+      section: req.body.section,
+      date: new Date(req.body.date + ' ' + req.body.time)
+    }).save()
+      .then(function () {
         return Game.aggregate([{
-            $match: {
-              $or: [{
-                'winner': winner
-              }, {
-                'loser': winner
-              }, {
-                'winner': loser
-              }, {
-                'loser': loser
-              }]
-            }
-          }, {
-            $group: {
-              _id: null,
-              winLoseCount: {
-                '$sum': {
-                  '$cond': [{
-                    $eq: ['$loser', winner]
-                  }, 1, 0]
-                }
-              },
-              winWinCount: {
-                '$sum': {
-                  '$cond': [{
-                    $eq: ['$winner', winner]
-                  }, 1, 0]
-                }
-              },
-              loseLoseCount: {
-                '$sum': {
-                  '$cond': [{
-                    $eq: ['$loser', loser]
-                  }, 1, 0]
-                }
-              },
-              loseWinCount: {
-                '$sum': {
-                  '$cond': [{
-                    $eq: ['$winner', loser]
-                  }, 1, 0]
-                }
+          $match: {
+            $or: [{
+              'winner': winner
+            }, {
+              'loser': winner
+            }, {
+              'winner': loser
+            }, {
+              'loser': loser
+            }]
+          }
+        }, {
+          $group: {
+            _id: null,
+            winLoseCount: {
+              '$sum': {
+                '$cond': [{
+                  $eq: ['$loser', winner]
+                }, 1, 0]
+              }
+            },
+            winWinCount: {
+              '$sum': {
+                '$cond': [{
+                  $eq: ['$winner', winner]
+                }, 1, 0]
+              }
+            },
+            loseLoseCount: {
+              '$sum': {
+                '$cond': [{
+                  $eq: ['$loser', loser]
+                }, 1, 0]
+              }
+            },
+            loseWinCount: {
+              '$sum': {
+                '$cond': [{
+                  $eq: ['$winner', loser]
+                }, 1, 0]
               }
             }
-          }])
+          }
+        }])
           .exec()
       })
-      .then(function(results) {
+      .then(function (results) {
         Team.update({
           _id: loser
         }, {
@@ -223,7 +223,7 @@ router.route('/addGame')
         }, {}).exec()
         res.redirect('/game')
       })
-      .catch(function(err) {
+      .catch(function (err) {
         next(err)
       })
   })
--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,6 @@
 module.exports = function (req, res, next) {
-  // if user is authenticated in the session, carry on
   if (req.isAuthenticated()) {
     return next()
   }
-  // if they aren't redirect them to the home page
   res.redirect('/auth/login')
 }
